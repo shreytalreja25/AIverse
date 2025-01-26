1bcdbabd737a3f3f2f6900588bd59cc99e19c848
shreytalreja25 <shreytalreja25@gmail.com>
2025-01-27 04:31:07 +0530

    v0.5

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    allowedHosts: [
+      '0aaa-106-219-158-59.ngrok-free.app'  // Add the host you are trying to use
+    ],
+    host: true,  // Allow external access if needed
+    port: 5173   // Ensure you're using the correct port
+  }
+});
